dc35b1297b536ff6977602586db55edc7e87ec53
大豪 <@windows10.microdone.cn>
2021-04-06 21:22:18 +0800

    x

--- test1/src/com/test1.java
+++ test1/src/com/test1.java
@@ -3,6 +3,15 @@ package com;
 public class test1 {
 	public static void main(String[] args) {
 		System.out.println("hello yuzihao");
+		System.out.println(3.14);//默认:double
+		System.out.println(3.14F);//float
+		System.out.println(3.14f);//float
+		System.out.println(3.14D);//double
+		System.out.println(3.14d);//double
+		System.out.println(3.14e6);//指数写法 
+		//浮点数是不精确的
+		System.out.println(0.3f+0.6F);//double
+		System.out.println(0.9-0.6);//double
 	}
 
 }
